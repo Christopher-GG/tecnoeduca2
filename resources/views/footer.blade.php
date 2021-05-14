0c51358aff2b9de8843322d813b874b88c57fbf4
Christopher G.G <cgg_chan@outlook.com>
2021-05-13 21:55:49 -0500

    Iniciando repositorio

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,47 @@
+<footer id="footer" class="pb-4 pt-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg">
+                <div class="row">
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <h5>Contacto</h5>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <small>5 de mayo #545</small>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <small>Tel. 4741234567</small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-lg">
+                <a href="#">Preguntas frecuentes</a>
+            </div>
+
+            <div class="col-12 col-lg">
+                <a href="#">Terminos y condiciones</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div id="footerDivider" class="pt-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <p>CopyRight @ 2021 TecnoEduca</p>
+            </div>
+
+            <div class="col-12 col-lg-6 text-lg-right text-left">
+                <a href="#">Noticias de privacidad</a>
+            </div>
+        </div>
+    </div>
+</div>
